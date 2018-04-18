0beacdda3ca3acccae662b2057e28237b8af2050
RadoszSzymon <radoszszymon@gmail.com>
2018-04-18 21:46:02 +0200

    heroku deployment stuff

--- config/database.php
+++ config/database.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+
+============= heroku ================
+
+*/
+$url = parse_url(getenv("mysql://b61d793ced2389:3d0ba98b@us-cdbr-iron-east-05.cleardb.net/heroku_6fc8af149d2d837?reconnect=true"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
+/*
+
+============= heroku ================
+
+*/
+
 return [
 
     /*
@@ -39,7 +57,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
@@ -52,7 +70,28 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+        ],*/
+
+        /*
+            ====================== heroku =====================
+        */
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', $host),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
+        /*
+            ====================== heroku =====================
+        */
 
         'pgsql' => [
             'driver' => 'pgsql',
